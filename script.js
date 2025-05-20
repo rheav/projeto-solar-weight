325be9a11fd2e7028786bd362a7ddfff1824a60d
Victor Rhea <victor.rhea@victor.rhea-J2JHJ2CMJJ>
2025-05-19 23:49:20 -0300

    quase movePlanet feito

--- script.js
+++ script.js
@@ -34,11 +34,16 @@ const getPreviousPlanet = () => {
 
 const movePlanet = (event) => {
   console.log(event.target.getAttribute("data-planet"));
-  if(event.target.getAttribute("data-planet") === planetList
+  console.log(planetList.indexOf(event.target.getAttribute("data-planet")));
   //TODO -> Como fazer a lista de planetas rodar?
   //TODO --- Preciso que ao clicar no planeta ative a movimentação do outro, mas rodando a array planetList
 
   event.target.classList.add("move-planet");
+  console.log(
+    planetList[planetList.indexOf(event.target.getAttribute("data-planet")) + 1]
+  );
+
+  document.querySelector(["data-planet"]=planetList[planetList.indexOf(event.target.getAttribute("data-planet")) + 1]) // Quero puxar o proximo elemento aqui
 };
 
 //step Event Listeners
